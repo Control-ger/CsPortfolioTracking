8179120fbf2eb7a5a3ac19b38074a049c98369b1
Control-ger <MaikSemrau@t-online.de>
2026-02-10 23:37:01 +0100

    Update:  vite config ohne api key

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,27 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
+import path from "path"; // WICHTIG
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      "/api/csfloat": {
+        target: "https://csfloat.com/api/v1",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/csfloat/, ""),
+        headers: {
+          Authorization: "",
+          "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0",
+          Accept: "application/json",
+        },
+      },
     },
   },
 });
